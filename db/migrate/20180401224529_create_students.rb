e9aef182f2c813118529100cdf02c26ca16bdc7f
Megan MacQuarrie <megan@Megans-MacBook-Pro.local>
2018-04-01 19:14:24 -0400

    Add static assets, students resource, and configure template

---
+++ db/migrate/20180401224529_create_students.rb
@@ -0,0 +1,10 @@
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.text :first_name
+      t.text :last_name
+
+      t.timestamps
+    end
+  end
+end
